02b69a5efa98cd35bb766fd56802d580d87e8f84
Joanna <jmgsese@gmail.com>
2018-05-22 12:46:46 -0500

    Solve checkUsersValid

--- exercises.js
+++ exercises.js
@@ -124,3 +124,22 @@ function minMax(arr){
     }
 
     module.exports = getShortMessages
+
+//     # Task
+//
+// Return a function that takes a list of valid users, and returns a function that returns true if all of the supplied users exist in the original list of users.
+//
+// You only need to check that the ids match.
+
+function checkUsersValid(goodUsers) {
+  return function allUsersValid(submittedUsers) {
+    return submittedUsers.every((sUser) => {
+      return goodUsers.some((gUser) => {
+        return sUser.id = gUser.id;
+      })
+    })
+  };
+}
+
+
+  module.exports = checkUsersValid
