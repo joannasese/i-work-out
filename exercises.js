c30ffaa9a24bf10af73a36c50fd4571795c1f64e
Joanna <jmgsese@gmail.com>
2018-05-22 12:26:03 -0500

    Solve getShortMessages

--- exercises.js
+++ exercises.js
@@ -109,3 +109,18 @@ function minMax(arr){
     }
 
     module.exports = doubleAll
+
+//     # Task
+//
+// Use Array#filter to write a function called getShortMessages.
+//
+// getShortMessages takes an array of objects with '.message' properties and returns an array of messages that are less than < 50 characters long.
+//
+// The function should return an array containing the messages themselves, without their containing object.
+    function getShortMessages(messages) {
+      return messages.map(message => message.message).filter((message) => {
+        return message.length < 50;
+      });
+    }
+
+    module.exports = getShortMessages
